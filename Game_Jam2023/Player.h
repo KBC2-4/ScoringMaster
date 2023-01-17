d90a15f89f3dffff010ba3044c680a53bb4564ca
takeru-nakayoshi <107020808+takeru-nakayoshi@users.noreply.github.com>
2023-01-17 15:07:07 +0900

    回答処理

--- Game_Jam2023/Player.h
+++ Game_Jam2023/Player.h
@@ -3,7 +3,20 @@ class Player
 {
 private:
 	//クラス内で使う変数
+	const char * q[5] = { "2+3", "5*3", "6/3" };  //問題
+	
+	const char * a[3][2] = {//答え([正答][誤答])
+		{"5","6"},
+		{"15","8"},
+		{"2","3"},
+	};
 
+	int i;//QRand代入用
+	int j;//QAand代入用
+
+	int qr;//問題用乱数
+	int ar;//答え用乱数(〇,×)
+	
 
 public:
 	Player();
@@ -11,5 +24,7 @@ public:
 	//関数をここで宣言する
 	void Update();
 	void Draw();
+	int QRand();//問題用乱数
+	int ARand();//答え用乱数(〇,×)
 };
 
